57cfb78b5f6de5defb665c36908ef90b9e3c0da2
simple-gpt <simple-gpt@simple-gpts-MacBook-Pro.local>
2025-12-13 05:32:39 +0100

    refactor: simplify hero layout

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -3,6 +3,12 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import PageIllustration from "@/components/page-illustration";
+import Avatar01 from "@/public/images/avatar-01.jpg";
+import Avatar02 from "@/public/images/avatar-02.jpg";
+import Avatar03 from "@/public/images/avatar-03.jpg";
+import Avatar04 from "@/public/images/avatar-04.jpg";
+import Avatar05 from "@/public/images/avatar-05.jpg";
+import Avatar06 from "@/public/images/avatar-06.jpg";
 import { landingContent } from "@/content";
 
 export default function HeroHome() {
@@ -11,15 +17,63 @@ export default function HeroHome() {
     <section className="relative">
       <PageIllustration />
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="pb-24 pt-48 md:pb-32 md:pt-56">
-          <div className="pb-16 text-center md:pb-20">
+        <div className="pb-12 pt-32 md:pb-20 md:pt-40">
+          <div className="pb-12 text-center md:pb-16">
+            <div
+              className="mb-6 border-y [border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1]"
+              data-aos="zoom-y-out"
+            >
+              <div className="-mx-0.5 flex justify-center -space-x-3">
+                <Image
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar01}
+                  width={32}
+                  height={32}
+                  alt="Avatar 01"
+                />
+                <Image
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar02}
+                  width={32}
+                  height={32}
+                  alt="Avatar 01"
+                />
+                <Image
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar03}
+                  width={32}
+                  height={32}
+                  alt="Avatar 02"
+                />
+                <Image
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar04}
+                  width={32}
+                  height={32}
+                  alt="Avatar 03"
+                />
+                <Image
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar05}
+                  width={32}
+                  height={32}
+                  alt="Avatar 04"
+                />
+                <Image
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar06}
+                  width={32}
+                  height={32}
+                  alt="Avatar 05"
+                />
+              </div>
+            </div>
             <h1
-              className="mb-8 text-5xl font-bold leading-tight md:text-6xl"
+              className="mb-6 border-y text-5xl font-bold [border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1] md:text-6xl"
               data-aos="zoom-y-out"
               data-aos-delay={150}
             >
-              <span className="block">Den Kopf frei.</span>
-              <span className="block mt-2">Der Rest in AKLOW.</span>
+              {hero.title}
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -29,9 +83,9 @@ export default function HeroHome() {
               >
                 {hero.subtitle}
               </p>
-              <div>
+              <div className="relative before:absolute before:inset-0 before:border-y before:[border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1]">
                 <div
-                  className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center"
+                  className="relative mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center"
                   data-aos="zoom-y-out"
                   data-aos-delay={450}
                 >
@@ -64,7 +118,7 @@ export default function HeroHome() {
             data-aos="zoom-y-out"
             data-aos-delay={600}
           >
-            <div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl">
+            <div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl before:pointer-events-none before:absolute before:-inset-5 before:border-y before:[border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1] after:absolute after:-inset-5 after:-z-10 after:border-x after:[border-image:linear-gradient(to_bottom,transparent,--theme(--color-slate-300/.8),transparent)1]">
               <div className="relative mb-8 flex items-center justify-between before:block before:h-[9px] before:w-[41px] before:bg-[length:16px_9px] before:[background-image:radial-gradient(circle_at_4.5px_4.5px,var(--color-gray-600)_4.5px,transparent_0)] after:w-[41px]">
                 <span className="text-[13px] font-medium text-white">
                   cruip.com
